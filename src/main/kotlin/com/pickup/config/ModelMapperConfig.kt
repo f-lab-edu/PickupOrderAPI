de68d9d176a313b4a57c0789256769488dff342c
jeonga <jeonga>
2023-08-06 16:20:24 +0900

    ModelMapper 추가

---
+++ src/main/kotlin/com/pickup/config/ModelMapperConfig.kt
@@ -0,0 +1,18 @@
+package com.pickup.config
+
+import org.modelmapper.ModelMapper
+import org.modelmapper.convention.MatchingStrategies
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ModelMapperConfig {
+
+    @Bean
+    fun modelMapper(): ModelMapper {
+        val modelMapper = ModelMapper()
+        modelMapper.configuration
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+        return modelMapper
+    }
+}
